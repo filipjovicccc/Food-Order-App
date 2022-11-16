41dcea1a46a6d525056d9d5f775dc7d8e869b395
filipjovicccc <filipjovicccc@gmail.com>
2022-11-16 22:50:19 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header />
+        <main>
+          <Meals />
+        </main>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
